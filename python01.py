f5fcf6e86959b4825c3f0fa9dede94a3cc1bc996
fahlev-dev <rfahlevi501@gmail.com>
2024-01-11 10:41:48 +0700

    membuat pertukaran nilai antar variabel

--- python01.py
+++ python01.py
@@ -1,2 +1,21 @@
 # menukar nilai antar variabel
 
+a, b = 10, 20
+print('sebelum pertukaran')
+print(f'a={a}, b={b}')
+
+a, b = b, a
+print('setelah pertukaran')
+print(f'a={a}, b={b}')
+
+print('====================')
+
+a, b, c, d, e = 10, 20, 30, 40, 50
+
+print('sebelum pertukaran')
+print(f'a={a}, b={b}, c={c}, d={d}, e={e}')
+
+a, b, c, d, e = c, e, d, a, b
+
+print('setelah pertukaran')
+print(f'a={a}, b={b}, c={c}, d={d}, e={e}')
