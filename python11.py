bb4dc1a7beba3630a8d7cbfbd74438f272dd5065
fahlev-dev <rfahlevi501@gmail.com>
2024-01-11 15:26:46 +0700

    memadukan list comprehension

---
+++ python11.py
@@ -0,0 +1,7 @@
+# memadukan list comprehension dan conditional IF di python
+
+listmu = [30, 60, 10, 55]
+listku = [item for item in listmu if item > 50]
+
+print(f'listmu: {listmu}')
+print(f'listku: {listku}')
