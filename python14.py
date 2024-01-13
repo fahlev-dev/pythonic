28559051715b07b8f72efc7de16c97f9c5c5d030
fahlev-dev <rfahlevi501@gmail.com>
2024-01-13 20:55:51 +0700

    membalikan urutan list dan string

---
+++ python14.py
@@ -0,0 +1,11 @@
+# membalikan urutan list dan string
+
+listmu = [10, 20, 30, 40, 50, 60]
+listku = listmu[::-1]
+    
+print(f'listmu: {listmu}')
+print(f'listku: {listku}')
+
+kata = 'BELAJAR'
+
+print(kata[::-1])
