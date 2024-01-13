39afcd91e546a4c5c7f21678bdae2798afe5df1f
fahlev-dev <rfahlevi501@gmail.com>
2024-01-13 21:23:44 +0700

    menggabungkan sekumpulan kata menjadi kalimat

---
+++ python16.py
@@ -0,0 +1,7 @@
+# menggabungkan sekumpulan kata menjadi kalimat (string concatenation) di python
+
+listku = ['belajar', 'python', 'dasar']
+kalimat = ' '.join(listku)
+   
+print(f'listku: {listku}')
+print(f'kalimat: {kalimat}')
