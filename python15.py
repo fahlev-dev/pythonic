ff61ca6df7b5c4c4b30a1b0b23278963884e4340
fahlev-dev <rfahlevi501@gmail.com>
2024-01-13 21:13:47 +0700

    memecah sekumpulan kata dalam kalimat ke dalam list

---
+++ python15.py
@@ -0,0 +1,7 @@
+# memecah sekumpulan kata dalam kalimat ke dalam list (splitting a string) di python
+
+teks = 'belajar python dasar'
+listku = teks.split()
+
+print(f'teks: {teks}')
+print(f'listku: {listku}')
