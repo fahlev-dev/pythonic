af6fbba9dae2900c79996a84db95da4dc4c24426
fahlev-dev <rfahlevi501@gmail.com>
2024-01-13 20:41:08 +0700

    belajar memotong list dan string

---
+++ python13.py
@@ -0,0 +1,13 @@
+# memotong list dan string
+
+listmu = [10, 20, 30, 40, 50, 60]
+listku = listmu[2:5]
+
+print(f'listku{listku}')
+
+kata = 'PEMBELAJARAN'
+
+print(len(kata))
+
+print(kata[3:10])
+
