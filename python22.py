693f912d234df0e573a59a33d957783a09510bb3
fahlev-dev <rfahlevi501@gmail.com>
2024-01-13 23:03:42 +0700

    belajar operator in di python

---
+++ python22.py
@@ -0,0 +1,9 @@
+# mengenal operator IN untuk memeriksa keadaan suatu nilai dalam list di python
+
+listku = [10, 20, 30, 40, 50]
+cari = 30
+
+if cari in listku:
+    print('ditemukan')
+else:
+    print('tidak ditemukan')
