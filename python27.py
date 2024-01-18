97e6daa4f2460e2fb67603e95313a5c72886c674
fahlev-dev <rfahlevi501@gmail.com>
2024-01-18 10:42:06 +0700

    belajar method count di python

---
+++ python27.py
@@ -0,0 +1,9 @@
+# mengenal method count di python untuk menghitung jumlah kemunculan suatu nilai
+
+listku = [30, 20, 60, 40, 20, 10, 30, 20]
+hitung = 20
+jumlah = listku.count(hitung)
+
+
+        
+print(f'angka {hitung}, ditemukan sebanyak {jumlah} buah, di dalam list {listku}')
