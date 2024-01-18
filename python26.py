4a2801497a97b2917f4979d8cbfdd71a4ef34b46
fahlev-dev <rfahlevi501@gmail.com>
2024-01-18 10:08:26 +0700

    belajar context manager di python

---
+++ python26.py
@@ -0,0 +1,5 @@
+# mengelola sumber daya dengan context manager di python
+
+with open('pesan2.txt', 'w') as fileku:
+    fileku.write('belajar python terbaru\n')
+    fileku.write('di indonesia belajar')
