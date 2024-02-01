06179f2dfa7281fffac5bb9789344d7460da561b
fahlev-dev <rfahlevi501@gmail.com>
2024-02-01 13:49:37 +0700

    belajar menggunakan method untuk string di python

---
+++ python39.py
@@ -0,0 +1,6 @@
+# mengenal dan merangkai beberapa method string pada bahasa pemrograman python
+
+x = ' Bahasa Pemrograman Python: guido Van RoSSum '
+print(f"x : {x}")
+y = x.upper().upper().replace(':', ';')
+print(f"y : {y}")
