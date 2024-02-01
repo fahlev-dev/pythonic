a42651950653d25202b0407d69e0f3882487cced
fahlev-dev <rfahlevi501@gmail.com>
2024-02-01 12:02:06 +0700

    belajar keyword argument di python

---
+++ python37.py
@@ -0,0 +1,6 @@
+# mengenal keyword arguments dan positional arguments pada bahasa python
+
+def cetak_nama(nama_depan, nama_belakang):
+    print(f"{nama_depan} {nama_belakang}")
+
+cetak_nama(nama_depan="karti", nama_belakang="susanti")
